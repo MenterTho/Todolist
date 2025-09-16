a6632e61cd992f6d1c41c8822076dd9ca0e991c7
MenterTho <kaosks123@gmail.com>
2025-09-16 22:03:08 +0700

    done test describe auth & not yet done code Integration Test v1

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -4,8 +4,8 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  testMatch: ["**/?(*.)+(spec|test).ts"],
-  setupFilesAfterEnv: ["<rootDir>/tests/setup.ts"],
+  testMatch: ["**/?(*.)+(spec|test).[tj]s"],
+  setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.ts"],
   coverageDirectory: "coverage",
   collectCoverageFrom: ["src/**/*.{ts,tsx,js,jsx}"],
   coverageReporters: ["text", "lcov"],
@@ -14,7 +14,7 @@ const config: Config = {
     "^.+\\.(ts|tsx)$": [
       "ts-jest",
       {
-        tsconfig: "tsconfig.json",
+        tsconfig: "tsconfig.json"
       },
     ],
   },
--- backend/src/tests/jest.setup.ts
+++ backend/src/tests/jest.setup.ts
@@ -7,6 +7,7 @@ import { Project } from "../modules/project/model/project.model";
 import { Workspace } from "../modules/workspace/model/workspace.model";
 import { UserWorkspace } from "../modules/user_workspace/model/user_workspace.model";
 import { Session } from "../modules/auth/model/session.model";
+import { Comment } from "../modules/comment/model/comment.model";
 
 dotenv.config();
 
@@ -35,7 +36,18 @@ afterEach(async () => {
     await repository.query(`DELETE FROM ${entity.tableName}`);
   }
 });
-
+jest.mock("nodemailer", () => ({
+  createTransport: jest.fn().mockReturnValue({
+    sendMail: jest.fn().mockResolvedValue({}),
+  }),
+}));
+jest.mock("cloudinary", () => ({
+  v2: {
+    uploader: {
+      upload: jest.fn().mockResolvedValue({ secure_url: "http://mocked.url" }),
+    },
+  },
+}));
 jest.mock("firebase-admin", () => ({
   messaging: () => ({
     send: jest.fn().mockResolvedValue("sent"),
